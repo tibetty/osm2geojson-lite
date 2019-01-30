a9f927d14d09e8b3b06790e4e91b93422e1d6951
git <xihua.duan@tourank.com>
2019-01-30 10:56:16 +0800

    1. refactoring: let => const; 2. remove otg results from test

--- test/test.js
+++ test/test.js
@@ -1,27 +1,20 @@
 #!/usr/bin/env node
 
 const fs = require('fs'),
-	DOMParser = require('xmldom').DOMParser,
-	osmtogeojson = require('osmtogeojson'),
 	osm2geojson = require('../.');
 
-console.log('========== xml conversion results side by side ==========');
+console.log('========== xml conversion results ==========');
 const xmlFiles = ['zhucheng.osm', 'hebei.osm', 'tokyodo.osm', 'usa.osm', 'original.osm'];
 for (let file of xmlFiles) {
 	let osm = fs.readFileSync(`./data/${file}`, 'utf-8');
-	console.log(`---------- osm2geojson-lite from ${file} ----------`);
+	console.log(`---------- result converted from ${file} ----------`);
 	console.log(JSON.stringify(osm2geojson(osm, {completeFeature: true})));
-	console.log(`---------- osmtogeojson from ${file} ----------`);
-	const osmdom = new DOMParser().parseFromString(osm);
-	console.log(JSON.stringify(osmtogeojson(osmdom)));
 }
 
-console.log('========== json conversion results side by side ==========');
+console.log('========== json conversion results  ==========');
 const jsonFiles = ['herne.json', 'empty.json', 'node.json', 'way.json', 'relation.json', 'map.json'];
 for (let file of jsonFiles) {
 	let osm = require(`./data/${file}`);
-	console.log(`---------- osm2geojson-lite from ${file} ----------`);
+	console.log(`---------- result converted from ${file} ----------`);
         console.log(JSON.stringify(osm2geojson(osm, {completeFeature: true})));
-        console.log(`---------- osmtogeojson from ${file} ----------`);
-	console.log(JSON.stringify(osmtogeojson(osm)));
 }
