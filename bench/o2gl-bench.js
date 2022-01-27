045e0391220c009c32344e69bbec1c0c89bbe35a
tibetty <xihua.duan@alibaba-inc.com>
2022-01-27 11:50:46 +0800

    removed vulnerable dependencies

--- bench/o2gl-bench.js
+++ bench/o2gl-bench.js
@@ -8,24 +8,24 @@ console.log('==========xml processing performance results==========');
 const xmlFiles = ['zhucheng.osm', 'hebei.osm', 'tokyodo.osm', 'usa.osm'];
 for (let file of xmlFiles) {
 	let osm = fs.readFileSync(`./data/${file}`, 'utf-8');
-	console.log(`---processing time for ${file}---`);
+	console.log(`---processing ${file}---`);
 	let stime = new Date().getTime();
 	for (let i = 0; i < rounds; i++) {
 		osm2geojson(osm, {completeFeature: true});
 	}
 	let etime = new Date().getTime();
-	console.log(`.${etime - stime}ms was taken for ${rounds} rounds`);
+	console.log(`-> ${etime - stime}ms was taken for ${rounds} rounds`);
 }
 
 console.log('==========json processing performance results==========');
 const jsonFiles = ['zhucheng.json', 'hebei.json', 'tokyodo.json', 'usa.json'];
 for (let file of jsonFiles) {
 	let osm = require(`./data/${file}`);
-	console.log(`---processing time for ${file}---`);
+	console.log(`---processing ${file}---`);
 	let stime = new Date().getTime();
 	for (let i = 0; i < rounds; i++) {
 		osm2geojson(osm, {completeFeature: true});
 	}
 	etime = new Date().getTime();
-	console.log(`.${etime - stime}ms was taken for ${rounds} rounds`);
+	console.log(`-> ${etime - stime}ms was taken for ${rounds} rounds`);
 }
