0418c6d67f785d39bf6de3d938c24a65498465d4
Xihua Duan <xihua.duan@gmail.com>
2025-03-03 13:01:10 +0800

    Update utils.ts

--- ts/src/utils.ts
+++ ts/src/utils.ts
@@ -102,11 +102,11 @@ export const strToFloat = (el: any[] | string): any => el instanceof Array ? el.
 
 export class LateBinder {
     private container: any[] | { [k: string]: any };
-    private valueFunc: (...args) => any;
+    private valueFunc: (...args: any[]) => any;
     private ctx: any;
     private args: any[];
 
-    constructor(container: any[] | { [k: string]: any }, valueFunc: (...args) => any, ctx: any, args: any[]) {
+    constructor(container: any[] | { [k: string]: any }, valueFunc: (...args: any[]) => any, ctx: any, args: any[]) {
         this.container = container;
         this.valueFunc = valueFunc;
         this.ctx = ctx;
@@ -118,19 +118,20 @@ export class LateBinder {
         if (this.container instanceof Array) {
             const idx = this.container.indexOf(this);
             if (idx >= 0) {
-                const args = [idx, 1];
+                const args: [number, number, any?] = [idx, 1];
                 if (v) {
                     args.push(v);
                 }
-                [].splice.apply(this.container, args);
+                Array.prototype.splice.apply(this.container, args);
             }
-        } else if (typeof this.container === 'object') {
-            const k = Object.keys(this.container).find((nv) => this.container[nv] === this);
+        } else if (typeof this.container === 'object' && !Array.isArray(this.container)) {
+            const container = this.container as { [k: string]: any };
+            const k = Object.keys(container).find((nv) => container[nv] === this);
             if (k) {
                 if (v) {
-                    this.container[k] = v;
+                    container[k] = v;
                 } else {
-                    delete this.container[k];
+                    delete container[k];
                 }
             }
         }
