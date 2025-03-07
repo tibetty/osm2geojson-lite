6f8c68c08486448f9314ce2e6ff8a637cf9a03fa
Xihua Duan <xihua.duan@gmail.com>
2025-03-07 16:43:21 +0800

    removed unused code

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
+WEB_TARGET_DIR = dist/web
+
 ifeq ($(OS),Windows_NT)
+    MKDIR_CMD = if not exist $(TARGET_DIR) mkdir $(subst /,\,$(WEB_TARGET_DIR))
     OPEN_CMD = start
     CP_CMD = copy
+    
 else
+    MKDIR_CMD = mkdir -p $(WEB_TARGET_DIR)
     UNAME_S := $(shell uname -s)
     ifeq ($(UNAME_S),Linux)
         OPEN_CMD = xdg-open
@@ -12,13 +17,16 @@ else
     CP_CMD = cp
 endif
 
-all: node web
+all: node create_web_dir web
 
 # New target for tests only
 test: test-node test-web
 
 node: ts/src/*
 	@cd ts && tsc --declaration && $(CP_CMD) src/polytags.json ../dist/node && cd ..
+
+create_web_dir:
+	@$(MKDIR_CMD)
 
 web: dist/node/index.js
 	@npx browserify -s osm2geojson dist/node/index.js | npx uglifyjs -c -m -o dist/web/osm2geojson-lite.js
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const fs = require('fs'),
 	osm2geojson = require('../dist/node');
 
 console.log('========== xml conversion results ==========');
-const xmlFiles = ['zhucheng.osm', 'hebei.osm', 'tokyodo.osm', 'usa.osm', 'original.osm'];
+const xmlFiles = ['zhucheng.osm', 'hebei.osm', 'tokyodo.osm', 'usa.osm', 'original.osm', 'route.osm'];
 for (let file of xmlFiles) {
 	let osm = fs.readFileSync(`./data/${file}`, 'utf-8');
 	console.log(`---------- result converted from ${file} ----------`);
@@ -12,7 +12,7 @@ for (let file of xmlFiles) {
 }
 
 console.log('========== json conversion results  ==========');
-const jsonFiles = ['herne.json', 'empty.json', 'node.json', 'way.json', 'relation.json', 'map.json'];
+const jsonFiles = ['herne.json', 'empty.json', 'node.json', 'way.json', 'relation.json', 'map.json', 'way2.json'];
 for (let file of jsonFiles) {
 	let osm = require(`./data/${file}`);
 	console.log(`---------- result converted from ${file} ----------`);
