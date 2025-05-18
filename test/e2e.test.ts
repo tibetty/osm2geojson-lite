05bd6942ec3418ef815c08808b5fee2894b76775
HarelM <harel.mazor@gmail.com>
2025-05-18 22:20:07 +0300

    Make tests easier to maintain, added missing expected files.

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -3,21 +3,32 @@ import osm2geojson from '../ts/src/index';
 import { describe, it, expect } from 'vitest';
 
 describe('osm2geojson', () => {
-    for (let xmlFile of ['zhucheng.osm', 'hebei.osm', 'tokyodo.osm', 'usa.osm', 'original.osm', 'route.osm']) {
+    let osmFiles = fs.readdirSync('./test/data').filter((file) => file.endsWith('.osm'));
+    for (let xmlFile of osmFiles) {
         it('should convert OSM XML to GeoJSON ' + xmlFile, () => {
             let osm = fs.readFileSync(`./test/data/${xmlFile}`, 'utf-8');
             let geojson = osm2geojson(osm, { completeFeature: true });
-            let expectedGeojson = JSON.parse(fs.readFileSync(`./test/expected/${xmlFile}.geojson`, 'utf-8'));
-            expect(geojson).toEqual(expectedGeojson);
+            const outputFile = `./test/expected/${xmlFile}.geojson`;
+            if (!fs.existsSync(outputFile)) {
+                fs.writeFileSync(outputFile, JSON.stringify(geojson, null, 2));
+            } else {
+                let expectedGeojson = JSON.parse(fs.readFileSync(outputFile, 'utf-8'));
+                expect(geojson).toEqual(expectedGeojson);
+            }
         });
     }
-
-    for (let jsonFile of ['herne.json', 'empty.json', 'node.json', 'way.json', 'relation.json', 'map.json', 'way2.json']) {
+    let jsonFiles = fs.readdirSync('./test/data').filter((file) => file.endsWith('.json'));
+    for (let jsonFile of jsonFiles) {
         it('should convert OSM JSON to GeoJSON ' + jsonFile, () => {
             let osm = JSON.parse(fs.readFileSync(`./test/data/${jsonFile}`, 'utf-8'));
             let geojson = osm2geojson(osm, { completeFeature: true });
-            let expectedGeojson = JSON.parse(fs.readFileSync(`./test/expected/${jsonFile}.geojson`, 'utf-8'));
-            expect(geojson).toEqual(expectedGeojson);
+            const outputFile = `./test/expected/${jsonFile}.geojson`;
+            if (!fs.existsSync(outputFile)) {
+                fs.writeFileSync(outputFile, JSON.stringify(geojson, null, 2));
+            } else {
+                let expectedGeojson = JSON.parse(fs.readFileSync(outputFile, 'utf-8'));
+                expect(geojson).toEqual(expectedGeojson);
+            }
         });
     }
 });
